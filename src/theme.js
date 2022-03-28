7033d4f66611d02b6257eec60623376b8ebea465
Atila <atilafb77@gmail.com>
2022-03-28 18:32:57 -0300

    add theme.js file with all required properties

---
+++ src/theme.js
@@ -0,0 +1,40 @@
+const colors = {
+  black: '#000000',
+  white: '#FFFFFF',
+  red: '#FF0000',
+  green: '#00FF00',
+  blue: '#0000FF',
+  yellow: '#FFFF00',
+  gray: '#808080',
+  purple: '#800080'
+}
+
+const space = [0, 4, 8, 16, 32, 64, 128, 256]
+
+const fonts = {
+  body:
+    'Roboto'
+}
+
+const fontSizes = [0, 8, 10, 12, 14, 16, 18, 20, 24, 28, 32, 36, 40]
+
+const fontWeights = [0, 300, 400, 500, 600]
+
+const radii = [0, 2]
+
+const zIndices = []
+
+const sizes = []
+
+const theme = {
+  colors,
+  space,
+  fonts,
+  fontSizes,
+  fontWeights,
+  radii,
+  zIndices,
+  sizes
+}
+
+export default theme
