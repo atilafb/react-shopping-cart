15035714122cdaa95a3eb835c7bb9f29cbe6d112
Atila <atilafb77@gmail.com>
2022-04-04 15:45:53 -0300

    fix:theme.js file

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,9 @@
 const colors = {
-  black: '#000000',
-  buttonDisabledTextColor: '#00000042',
-  buttonDisabledBackground: '#0000001f',
+  black: {
+    standard: '#000000',
+    opacityDotOne: '#0000001f',
+    opacityDotTwo: '#00000042'
+  },
   white: '#FFFFFF',
   red: '#FF0000',
   green: '#00FF00',
@@ -13,17 +15,17 @@ const colors = {
   strongCyan: '#1565c0',
 }
 
-const space = [0, 4, 8, 16, 32, 64, 128, 256]
+const space = ['0px', '4px', '8px', '16px', '32px', '64px', '128px', '256px']
 
 const fonts = {
   body: "'Roboto', 'Helvetica', 'Arial', sans-serif"
 }
 
-const fontSizes = [0, 8, 10, 12, 14, 16, 18, 20, 24, 28, 32, 36, 40]
+const fontSizes = ['0px', '8px', '10px', '12px', '14px', '16px', '18px', '20px', '24px', '28px', '32px', '36px', '40px']
 
 const fontWeights = [0, 300, 400, 500, 600]
 
-const radii = [0, 2, 4]
+const radii = ['0px', '2px', '4px']
 
 const zIndices = []
 
